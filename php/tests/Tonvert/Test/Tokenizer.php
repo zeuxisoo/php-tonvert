7faf454cd5ae50e49fd034ea8fc54476148d01f8
Zeuxis Lo <seekstudio@gmail.com>
2017-06-10 17:05:01 +0800

    update and add take method test case

--- php/tests/Tonvert/Test/Tokenizer.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Tonvert\Test;
-
-use Tonvert\TestCase;
-
-class TokenizerTest extends TestCase {
-
-}
---
+++ php/tests/Tonvert/Test/TokenizerTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Tonvert\Test;
+
+use Tonvert\TestCase;
+use Tonvert\Tokenizer;
+use Tonvert\Model\Token;
+
+class TokenizerTest extends TestCase {
+
+    public function testTake() {
+        $tokenizer = new Tokenizer();
+        $tokenes   = $tokenizer->take(__DIR__."/Fixtures/default.txt");
+
+        $this->assertEquals([
+            new Token(Token::TYPE_PARENTHESES_OPEN, '('),
+            new Token(Token::TYPE_NAME, 'subtract'),
+            new Token(Token::TYPE_NUMBER, '2017'),
+            new Token(Token::TYPE_STRING, 'ff68'),
+            new Token(Token::TYPE_PARENTHESES_OPEN, '('),
+            new Token(Token::TYPE_NAME, 'add'),
+            new Token(Token::TYPE_NUMBER, '6'),
+            new Token(Token::TYPE_NUMBER, '10'),
+            new Token(Token::TYPE_PARENTHESES_CLOSE, ')'),
+            new Token(Token::TYPE_PARENTHESES_CLOSE, ')'),
+        ], $tokenes);
+    }
+
+}
