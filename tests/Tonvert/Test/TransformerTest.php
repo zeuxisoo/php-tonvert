ab333a117ed617973a5eb6f21dcdbe2d06f815c5
Zeuxis Lo <seekstudio@gmail.com>
2017-06-20 15:49:12 +0800

    add new transformer test case for mult.txt

--- tests/Tonvert/Test/TransformerTest.php
+++ tests/Tonvert/Test/TransformerTest.php
@@ -42,4 +42,62 @@ class TransformerTest extends TestCase {
         $this->assertEquals($root, $newAst);
     }
 
+    public function testTransformMulti() {
+        $tokenizer = new Tokenizer();
+        $tokenes   = $tokenizer->take(__DIR__."/Fixtures/multi.txt");
+
+        $parser = new Parser();
+        $ast    = $parser->parse($tokenes);
+
+        $transformer = new Transformer();
+        $newAst      = $transformer->transform($ast);
+
+        $root = TransformedNode::factory(TransformedNode::TYPE_PROGRAM);
+        $root->setBody([
+            TransformedNode::factory(TransformedNode::TYPE_EXPRESSION_STATEMENT)
+                ->setExpression(
+                    TransformedNode::factory(TransformedNode::TYPE_CALL_EXPRESSION)
+                        ->setCallee(
+                            TransformedNode::factory(TransformedNode::TYPE_IDENTIFIER)
+                                ->setName("add")
+                        )
+                        ->setArguments([
+                            TransformedNode::factory(TransformedNode::TYPE_CALL_EXPRESSION)
+                                ->setCallee(
+                                    TransformedNode::factory(TransformedNode::TYPE_IDENTIFIER)
+                                        ->setName("subtract")
+                                )
+                                ->setArguments([
+                                    TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(2017),
+                                    TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(6),
+                                ]),
+                            TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(19)
+                        ])
+                ),
+
+            TransformedNode::factory(TransformedNode::TYPE_EXPRESSION_STATEMENT)
+                ->setExpression(
+                    TransformedNode::factory(TransformedNode::TYPE_CALL_EXPRESSION)
+                        ->setCallee(
+                            TransformedNode::factory(TransformedNode::TYPE_IDENTIFIER)
+                                ->setName("add")
+                        )
+                        ->setArguments([
+                            TransformedNode::factory(TransformedNode::TYPE_CALL_EXPRESSION)
+                                ->setCallee(
+                                    TransformedNode::factory(TransformedNode::TYPE_IDENTIFIER)
+                                        ->setName("subtract")
+                                )
+                                ->setArguments([
+                                    TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(2017),
+                                    TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(6),
+                                ]),
+                            TransformedNode::factory(TransformedNode::TYPE_NUMBER_LITERAL)->setValue(20)
+                        ])
+                ),
+        ]);
+
+        $this->assertEquals($newAst, $root);
+    }
+
 }
